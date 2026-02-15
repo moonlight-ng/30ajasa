308305f2a578075544da80cb7d013a8bf454a62a
Opemipo Aikomo <opemipoaikomo@gmail.com>
2026-02-15 12:12:21 +0100

    Foldering

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,34 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
+const rewritePlugin = () => {
+    return {
+        name: 'rewrite-middleware',
+        configureServer(server) {
+            server.middlewares.use((req, res, next) => {
+                if (req.url.startsWith('/makerspace')) {
+                    req.url = req.url.replace('/makerspace', '/pages/makerspace')
+                } else if (req.url.startsWith('/building')) {
+                    req.url = req.url.replace('/building', '/pages/building')
+                } else if (req.url.startsWith('/film-club')) {
+                    req.url = req.url.replace('/film-club', '/pages/film-club')
+                }
+                next()
+            })
+        }
+    }
+}
+
 export default defineConfig({
+    plugins: [rewritePlugin()],
     build: {
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
                 notFound: resolve(__dirname, '404.html'),
-                building: resolve(__dirname, 'building/index.html'),
-                filmClub: resolve(__dirname, 'film-club/index.html'),
-                makerspace: resolve(__dirname, 'makerspace/index.html'),
+                building: resolve(__dirname, 'pages/building/index.html'),
+                filmClub: resolve(__dirname, 'pages/film-club/index.html'),
+                makerspace: resolve(__dirname, 'pages/makerspace/index.html'),
             },
         },
     },
